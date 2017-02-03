8b35454f136c95247876215da5095fb7720dbbbc
Fachri Hawari <fachri.hawari@gmail.com>
2017-02-03 19:58:19 +0700

    Add middleware dir and update config

--- src/config/directory.ts
+++ src/config/directory.ts
@@ -1,13 +1,16 @@
 import * as path from 'path'
 
 let rootDir = path.dirname(__dirname)
+let appDir = path.join(rootDir, 'app')
 
 export default {
     config: path.join(rootDir, 'config'),
-    controller: path.join(rootDir, 'app/Controllers'),
-    model: path.join(rootDir, 'app/Models'),
+    controller: path.join(appDir, 'Controllers'),
+    middleware: path.join(appDir, 'Middlewares'),
+    model: path.join(appDir, 'Models'),
     view: path.join(rootDir, 'resources/views'),
+    lang: path.join(rootDir, 'resources/lang'),
     route: path.join(rootDir, 'routes'),
-    public: path.join(rootDir, 'public'),        
-    core: path.join(rootDir, 'core') 
+    public: path.join(rootDir, 'public'),
+    core: path.join(rootDir, 'core')
 }
